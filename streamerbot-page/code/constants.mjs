25d082a446ac73075e178f0ae6487650b145edf5
swolekat <swolecattwitch@gmail.com>
2024-10-15 00:23:51 -0500

    add bits

--- streamerbot-page/code/constants.mjs
+++ streamerbot-page/code/constants.mjs
@@ -1,3 +1,3 @@
 export const WEBSOCKET_URL = "ws://127.0.0.1:58585";
-export const BITS_TO_IGNORE = [300, 420, 500, 69];
+export const BITS_TO_IGNORE = [284, 285, 300, 420, 500, 69];
 export const CHEER_THRESHOLD = 100;
